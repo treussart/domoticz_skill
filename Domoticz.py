37975391f1656eea0e3dfbb0f53777256003de56
matthieu <matthieu@treussart.com>
2017-03-11 14:20:28 +0100

    update coverage

--- Domoticz.py
+++ Domoticz.py
@@ -1,3 +1,4 @@
+"""For controlling Domoticz."""
 import urllib
 import ConfigParser
 import os
@@ -8,8 +9,10 @@ LOGGER = getLogger(__name__)
 
 
 class Domoticz:
+    """Class for controlling Domoticz."""
 
     def __init__(self):
+        """Recover the config files for accessing to Domoticz instance."""
         config_name = "conf.cfg"
         config_file = os.path.join(os.path.dirname(__file__), config_name)
         self.config = ConfigParser.ConfigParser()
@@ -19,6 +22,7 @@ class Domoticz:
         self.url = "http://" + self.host + ":" + self.port
 
     def convert_name_to_idx(self, what, where):
+        """Convert the 'what' and the 'where', for recover the idx of the device in Domoticz."""
         if where is None:
             for (key, val) in self.config.items("devices"):
                 if what.lower().strip() == key.lower().strip():
@@ -30,6 +34,7 @@ class Domoticz:
         return 0
 
     def switch(self, state, idx):
+        """Switch the device in Domoticz."""
         try:
             f = urllib.urlopen(self.url + "/json.htm?type=command&param=switchlight&idx=" + str(idx) + "&switchcmd=" + str(state).title())
             response = f.read()
@@ -39,6 +44,7 @@ class Domoticz:
             LOGGER.error(str(e) + ' : ' + str(e.read()))
 
     def get(self, idx):
+        """Get the device's data in Domoticz."""
         try:
             f = urllib.urlopen(self.url + "/json.htm?type=devices&rid=" + str(idx))
             response = f.read()
